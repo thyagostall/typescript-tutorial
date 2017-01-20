716f8f17eb520bc473aa509efe3b7d1706145bb7
thyagostall <thstall@gmail.com>
2017-01-20 16:43:34 -0200

    Initial commit

---
+++ greeter.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Thyago";
+document.body.innerHTML = greeter(user);
---
+++ greeter.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+	return "Hello, " + person;
+}
+
+var user = "Thyago";
+
+document.body.innerHTML = greeter(user);
